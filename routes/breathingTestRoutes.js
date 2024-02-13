cb6463f550754af6131f93873eb9efecdcf6aa33
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-02-13 16:58:31 +0530

    pra-fea-breathing test api
    post get delete api for breathing test

---
+++ routes/breathingTestRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const {
+    getbreathingTestResult,
+    createBreathingTestResult,
+    deletebreathingTestResults
+}=require('../controllers/breathingTestController')
+
+const router = express.Router()
+
+//get all results
+router.get('/', getbreathingTestResult)
+
+//post a new result
+router.post('/', createBreathingTestResult)
+
+//delete results
+router.delete('/', deletebreathingTestResults)
+
+module.exports = router
