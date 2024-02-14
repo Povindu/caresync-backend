ccf3960b594fd8c775422551119d9c0e2658d4f5
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-02-14 07:51:07 +0530

    pra-fea-stepcountbackend

---
+++ controllers/stepCountController.js
@@ -0,0 +1,32 @@
+const stepCount = require('../models/stepCountModel')
+
+//get results
+const getStepCounterTestResult = async(req, res)=>{
+    const stepCountResult = await stepCount.find({}).sort({createdAt: -1})
+    res.status(200).json(stepCountResult)
+}
+
+//post result
+const createStepCountResult = async(req,res)=>{
+    const {date, stopwatchTime, step} = req.body
+
+    //add doc to db
+    try{
+        const stepCounter = await stepCount.create({date,stopwatchTime,step})
+        res.status(200).json(stepCounter)
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+//delete results
+const deleteStepCounterTestResult = async(req, res)=>{
+    const deleteResult = await stepCount.find({}).sort({createdAt: -1})
+    res.status(200).json(deleteResult)
+}
+
+module.exports={
+    getStepCounterTestResult,
+    createStepCountResult,
+    deleteStepCounterTestResult
+}
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const requireAuth = require("./middleware/requireAuth")
 // require('./models/UserNew');
 // const trackRoutes = require('./routes/trackRoutes');
 const BreathingTestRoutes = require('./routes/breathingTestRoutes')
+const StepCounterTestRoutes = require('./routes/stepCountTestRoutes')
 
 
 
@@ -36,8 +37,7 @@ app.get('/', requireAuth, (req, res) => {
 
 
 app.use('/api/breathingTests', BreathingTestRoutes)
-
-
+app.use('/api/stepCounterTests', StepCounterTestRoutes)
 
 
 //connect to db
