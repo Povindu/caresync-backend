0814df252dfe64a594bd357c38e98f41bc83cd44
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-03-10 15:00:09 +0530

    feat: add new routes on patients history

--- controllers/PatientHistoryController.js
+++ controllers/PatientHistoryController.js
@@ -1,13 +1,17 @@
 // backend/controllers/patientHistoryController.js
 
-const PatientHistory = require("../models/patientHistory");
+// const PatientHistory = require("../models/patientHistory");
+const Patient = require("../models/Patient");
+const PatientHistory = require("./../models/patientHistory");
 
 // Get all patients history
 const getPatientsHistory = async (req, res) => {
   try {
-    const patientsHistory = await PatientHistory.find({}).sort({
-      createdAt: -1,
-    });
+    console.log("Fetching patients history");
+    const patientsHistory = await PatientHistory.find();
+    // .sort({
+    //   createdAt: -1,
+    // });
     console.log(patientsHistory);
     res.status(200).json(patientsHistory);
   } catch (error) {
@@ -16,6 +20,29 @@ const getPatientsHistory = async (req, res) => {
   }
 };
 
+const enterData = async (req, res) => {
+  const { recordId, title, date, description, doctor, symptom, presId } =
+    await req.body;
+  console.log(req.body);
+  try {
+    const patientHistory = new PatientHistory({
+      recordId,
+      title,
+      date,
+      description,
+      doctor,
+      symptom,
+      presId,
+    });
+    const newPatientHistory = await patientHistory.save();
+    res.status(201).json(newPatientHistory);
+  } catch (error) {
+    console.error("Error entering patient history:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 module.exports = {
   getPatientsHistory,
+  enterData,
 };
--- routes/patientHistoryRoutes.js
+++ routes/patientHistoryRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   getPatientsHistory,
+  enterData,
 } = require("../controllers/PatientHistoryController");
 
 const router = express.Router();
 
 router.get("/", getPatientsHistory);
+router.post("/", enterData);
 
 module.exports = router;
